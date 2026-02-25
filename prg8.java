0850f35bc5b7c944a4c5eb71e24754a5214f057b
srujan1700 <srujan170305@gmail.com>
2026-02-25 22:55:08 +0530

    Add files via upload

---
+++ prg8.java
@@ -0,0 +1,14 @@
+package Package2;
+
+import java.util.*;
+class prg8{
+    static int fact(int n){
+        if(n==0) return 1;
+        return n*fact(n-1);
+    }
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        System.out.println(fact(n));
+    }
+}
---
+++ prg9.java
@@ -0,0 +1,18 @@
+package Package2;
+
+
+import java.util.*;
+class prg9{
+    static boolean isPalindrome(String s){
+        String r="";
+        for(int i=s.length()-1;i>=0;i--){
+            r+=s.charAt(i);
+        }
+        return s.equals(r);
+    }
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        String s=sc.next();
+        System.out.println(isPalindrome(s));
+    }
+}
